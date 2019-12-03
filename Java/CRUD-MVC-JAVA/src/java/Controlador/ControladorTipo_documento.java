318a5e066958e28ef6bc92cf22aa35ce81d62784
David Fagua <48558001+David-Fagua@users.noreply.github.com>
2019-12-03 18:54:57 -0500

    Versión 0.1.18 "Java 0.14"
    Creacion de proyecto HSD_PLUS
    Para implementacion de  Spring.
    Listo para implementar la fase de Samuel.
    
    Co-Authored-By: David Fagua <david-fagua@users.noreply.github.com>

---
+++ Java/CRUD-MVC-JAVA/src/java/Controlador/ControladorTipo_documento.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Tipo_documento;
+import ModeloDAO.Tipo_documentoDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author SAM
+ */
+public class ControladorTipo_documento extends HttpServlet {
+    String listar="vistas/tipodoc/listar.jsp";
+    String add="vistas/tipodoc/add.jsp";
+    String edit="vistas/tipodoc/edit.jsp";
+    Tipo_documento td=new Tipo_documento();
+    Tipo_documentoDAO dao=new Tipo_documentoDAO();
+    int id_tdocumento;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControladorTipo_documento</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ControladorTipo_documento at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acceso="";
+        String action=request.getParameter("accion");
+        if(action.equalsIgnoreCase("listar")){
+            acceso=listar;
+        }else if(action.equalsIgnoreCase("add")){
+            acceso=add;
+        }
+        else if(action.equalsIgnoreCase("Agregar")){
+            String abrebiatura=request.getParameter("txtabrebiatura");
+            String nombre=request.getParameter("txtnombre");
+            td.setAbrebiatura(abrebiatura);
+            td.setNombre(nombre);
+            dao.add(td);
+            acceso=listar;
+        }
+        else if(action.equalsIgnoreCase("editar")){
+            request.setAttribute("idtid", request.getParameter("id_tdocumento"));
+            acceso=edit;
+        }
+        else if(action.equalsIgnoreCase("Actualizar")){
+            id_tdocumento=Integer.parseInt(request.getParameter("txtid_tdocumento"));
+            String abrebiatura=request.getParameter("txtabrebiatura");
+            String nombre=request.getParameter("txtnombre");
+            td.setId_tdocumento(id_tdocumento);
+            td.setAbrebiatura(abrebiatura);
+            td.setNombre(nombre);
+            dao.edit(td);
+            acceso=listar;
+        }
+        else if(action.equalsIgnoreCase("eliminar")){
+            id_tdocumento=Integer.parseInt(request.getParameter("id_tdocumento"));
+            td.setId_tdocumento(id_tdocumento);
+            dao.eliminar(id_tdocumento);
+            acceso=listar;
+        }
+        RequestDispatcher vista=request.getRequestDispatcher(acceso);
+        vista.forward(request, response);
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Java/CRUD-MVC-JAVA/src/java/Modelo/Ciudad.java
@@ -0,0 +1,33 @@
+
+package Modelo;
+
+
+public class Ciudad {
+    int id_ciudad;
+    String nombre;
+
+    public Ciudad() {
+    }
+
+    public Ciudad(int id_ciudad, String nombre) {
+        this.id_ciudad = id_ciudad;
+        this.nombre = nombre;
+    }
+
+    public int getId_ciudad() {
+        return id_ciudad;
+    }
+
+    public void setId_ciudad(int id_ciudad) {
+        this.id_ciudad = id_ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
