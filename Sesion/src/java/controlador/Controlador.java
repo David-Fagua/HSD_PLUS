a364a5fa4ddcfa5ba46a2b50e5707b79348208f5
David Fagua <48558001+David-Fagua@users.noreply.github.com>
2020-01-24 21:57:39 -0500

    Versión 0.1.27 "cambio"

--- Sesion/src/java/controlador/Controlador.java
+++ Sesion/src/java/controlador/Controlador.java
@@ -18,29 +18,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Controlador extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String accion=request.getParameter("accion");
+        if(accion.equals("Ingresar")){
+            String nombre=request.getParameter("nombre");
+            String correo=request.getParameter("email");
+            p.setnombre(nombre);
+            p.setemail(correo);
+            dao.validar(p);
         }
     }
 
