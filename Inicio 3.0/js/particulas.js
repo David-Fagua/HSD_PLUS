94c773ee09823305dcfc738e0c07a2a80d08552a
David Fagua <48558001+David-Fagua@users.noreply.github.com>
2020-02-22 10:12:35 -0500

    Versión 0.1.30 "Diseño 3.2"
    -Se arregla un error

--- Inicio 3.0/js/particulas.js
+++ Inicio 3.0/js/particulas.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#47A5B4"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#47A5B4"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#47A5B4",
+        "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
       },
