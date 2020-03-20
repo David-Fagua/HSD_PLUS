74b5ac8fb73091737e3e58c6eec179ae4dafa7f8
David Fagua <48558001+David-Fagua@users.noreply.github.com>
2020-03-19 19:18:39 -0500

    Versión 0.4.4 "GF"

--- HSD_plus/src/java/edu/HSD_plus/converters/RolesConverter.java
+++ HSD_plus/src/java/edu/HSD_plus/converters/RolesConverter.java
@@ -12,6 +12,7 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import glimport org.glassfish.soteria.Utils;
 
 /**
  *
